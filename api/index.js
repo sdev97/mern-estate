cf7180c5f8dabd5ea714c88e5a0383f1a05d4913
sytv <transy97@gmail.conm>
2024-05-20 13:52:41 +0700

    [update] connect to db

--- api/index.js
+++ api/index.js
@@ -1,5 +1,13 @@
 import express from 'express'
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config()
 
+mongoose.connect(process.env.MONGO).then(() => {
+    console.log('Connected to MongoDB!')
+}).catch(error => {
+    console.log(error)
+})
 const app = express();
 
 app.listen(3000, () => {
