e1ceb1e589c44f315e65169b98aa85851538416f
sytv <transy97@gmail.conm>
2024-05-22 10:47:03 +0700

    create login api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from "../models/user.model.js"
 import bcryptjs from 'bcryptjs'
 import { errorHandler } from "../utils/error.js"
+import jwt from 'jsonwebtoken'
 
 export const signup = async (req, res, next) => {
     const { username, email, password } = req.body
@@ -21,6 +22,19 @@ export const signup = async (req, res, next) => {
             message: "error"
         })
     }
-   
-    
+}
+
+export const signin = async (req, res, next) => {
+    const {email, password} = req.body
+    try {
+        const validUser = await User.findOne({ email })
+        if (!validUser) return next(errorHandler(404, 'User not found!'));
+        const validPassword = bcryptjs.compareSync(password, validUser.password)
+        if (!validPassword) return next(errorHandler(401, 'Wrong cridential!'));
+        const token = jwt.sign({id: validUser._id}, process.env.SECRET_KEY)
+        const {password: pass, ...left} = validUser._doc
+        res.cookie('access_token', token, {httpOnly: true, expires: new Date(Date.now() + 60 * 60)}).status(200).json(left)
+    } catch (error) {
+        next(error)
+    }
 }
