0e2b8fc61e176caf9b93cd5cca1f65aaf076590d
sytv <transy97@gmail.conm>
2024-05-25 16:05:41 +0700

    create user update api

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,5 +1,34 @@
+import User from "../models/user.model.js"
+import { errorHandler } from "../utils/error.js"
+import bycyptjs from "bcryptjs"
+
 export const test = (req, res) => {
     res.json({
         message: 'Hello world'
     })
+}
+
+export const updateUser = async (req, res, next) => {
+    // console.log('11111', req.d)
+    if (req.user.id !== req.params.id) return next(errorHandler(401, 'Unauthorized'));
+    try {
+        if (req.body.password) {
+            req.body.password = bycyptjs.hashSync(req.body.password, 10)
+        }
+
+        const updatedUser = await User.findByIdAndUpdate(req.params.id, {
+            $set: {
+                // ...req.body,
+                username: req.body.username,
+                email: req.body.email,
+                password: req.body.password,
+                avatar: req.body.avatar
+            }
+        }, {new: true})
+
+        const {password, ...rest} = updatedUser._doc
+        res.status(200).json(rest)
+    } catch (error) {
+        next(error)
+    }
 }
--- api/index.js
+++ api/index.js
@@ -4,6 +4,7 @@ import dotenv from 'dotenv';
 // import userRouter from './routes/user.route.js'
 import getUser from './routes/user.route.js'
 import authRouter from './routes/auth.route.js'
+import cookieParser from 'cookie-parser';
 dotenv.config()
 
 mongoose.connect(process.env.MONGO).then(() => {
@@ -14,6 +15,8 @@ mongoose.connect(process.env.MONGO).then(() => {
 const app = express();
 
 app.use(express.json())
+
+app.use(cookieParser())
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000')
